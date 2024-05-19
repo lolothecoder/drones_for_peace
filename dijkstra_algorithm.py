26789f6dfad7c6ecc501e4c6c208c34cc0b423dc
Lolo <loicfinette@gmail.com>
2024-05-19 21:51:39 +0200

    dijkstra seems to work

--- dijkstra_algorithm.py
+++ dijkstra_algorithm.py
@@ -1,12 +1,31 @@
 from dijkstar import Graph, find_path
 from matplotlib import pyplot as plt
 import numpy
+import threading
+import time
 
-length = 0.1
+'''
+# Task to be executed at regular intervals.
+def timer():
+    while True:
+        print("Hello world!")
+        time.sleep(3)   # 3 seconds.
+'''
+# Start execution in the background.
+#t = threading.Thread(target=timer)
+#t.start()
+
+'''
+length = 0.15
 rows = 8
-columns = 4
+columns = 7
 start = 2
 goal = 28
+'''
+
+
+def print_msg():
+    print("Mmmmmm Selma")
 
 def update_connections(graph):
     nodes = list(graph.keys())
@@ -18,24 +37,29 @@ def update_connections(graph):
     return connections
 
 def remove(graph, node):
-    graph.remove_node(node)
-    connections = update_connections(graph)
-    return connections
+    #print(graph)
+    #print(type(node))
+    try:
+        graph.remove_node(node)
+        connections = update_connections(graph)
+        return connections
+    except:
+        return None
 
 def create_graph(rows, columns, length):
     nodes_points = []
     for i in range(rows):
         for y in range(columns):
-            nodes_points.append((round(length*y, 1), round(length*i, 1)))
+            nodes_points.append((round(length*y, 2), round(length*i, 2)))
 
     graph = Graph()
     for i in range(len(nodes_points)):
         if ((i < len(nodes_points) - 1) and (nodes_points[i][1] == nodes_points[i+1][1])):
-            graph.add_edge(i, i+1, 0.1)
-            graph.add_edge(i+1, i, 0.1)
+            graph.add_edge(i, i+1, length)
+            graph.add_edge(i+1, i, length)
         if (nodes_points[i][1] > 0):
-            graph.add_edge(i, i-4, 0.1)
-            graph.add_edge(i-4, i, 0.1)
+            graph.add_edge(i, i-columns, length)
+            graph.add_edge(i-columns, i, length)
 
     connections = update_connections(graph)
     return graph, nodes_points, connections 
@@ -47,16 +71,20 @@ def get_best_path(graph, node_points, start, goal):
     path = None
     while path is None:
         try:
+            print("trying")
             path = find_path(graph, start, goal)
         except:
             goal += 1
-            print("Changed goal")
+            if goal > len(graph): goal = 0
+            print("Changed goal : " + str(goal))
+        #print(path)
 
     for i in range(len(path.nodes)):
         if(i < len(path.nodes) - 1):
             best_path_edges.append([i,i+1])
         best_path_nodes.append(node_points[path.nodes[i]])
-    return best_path_edges, best_path_nodes
+    node_ids = path.nodes
+    return best_path_edges, best_path_nodes, node_ids
 
 def visualisations(node_points, connections, best_path_nodes, best_path_edges):
     points = numpy.array(node_points)
@@ -72,20 +100,69 @@ def visualisations(node_points, connections, best_path_nodes, best_path_edges):
     x_best = best_path_nodes[:,0].flatten()
     y_best = best_path_nodes[:,1].flatten()
 
+    plt.ion()
+    fig = plt.figure()
     plt.plot(x[edges.T], y[edges.T], 'y-') # Edges
     plt.plot(x, y, 'ro') # Points
 
     plt.plot(x_best[best_path_edges.T], y_best[best_path_edges.T], 'b-') # Edges
     plt.plot(x_best, y_best, 'bo') # Points
     plt.axis('equal')
-    plt.show()
+    #fig = plt.gcf() 
+    return fig
 
+def generate_dijkstra(rows, columns, length, start, goal):
+    graph, node_points, connections = create_graph(rows, columns, length)
+    best_path_edges, best_path_nodes, node_ids = get_best_path(graph, node_points, start, goal)
+    fig = visualisations(node_points, connections, best_path_nodes, best_path_edges)
+    return graph, node_points, connections, best_path_edges, best_path_nodes, fig, node_ids
+
+def conversion(node_points, height):
+    sequence = []
+    for i in range(len(node_points)):
+        sequence.append((node_points[i][1], -node_points[i][0], height))
+    return sequence
+
+def draw_map(fig):
+    fig.canvas.draw() 
+    fig.canvas.flush_events() 
+#graph, node_points, connections, best_path_edges, best_path_nodes = generate_dijkstra(rows, columns, length, start, goal)
+#print(best_path_nodes)
+#print(best_path_nodes[0][0], best_path_nodes[0][1])
+#converted_nodes = conversion(best_path_nodes, 0.4)
+#print(converted_nodes)
+#node_points = numpy.array([(1, 1), (2, 2)])
+#converted = conversion(node_points)
+#print(converted)
+
+#plt.close(fig)
+
+'''
 graph, node_points, connections = create_graph(rows, columns, length)
-connections = remove(graph, 8)
-connections = remove(graph, 9)
-connections = remove(graph, 13)
-connections = remove(graph, 20)
-best_path_edges, best_path_nodes = get_best_path(graph, node_points, start, goal)
-visualisations(node_points, connections, best_path_nodes, best_path_edges)
+#print(graph)
+#print(node_points)
+#print(connections)
+best_path_edges, best_path_nodes, node_ids = get_best_path(graph, node_points, start, goal)
+print(best_path_edges)
+print(best_path_nodes)
+print(node_ids)
+fig = visualisations(node_points, connections, best_path_nodes, best_path_edges)
+draw_map(fig)
+time.sleep(30)
 
-
+#plt.close(fig)
+#fig.close()
+#plt.show()
+#plt.close(fig)
+#connections = remove(graph, 8)
+#connections = remove(graph, 9)
+#connections = remove(graph, 13)
+#connections = remove(graph, 20)
+print("wohoo")
+#best_path_edges, best_path_nodes = get_best_path(graph, node_points, start, goal)
+print("good")
+#fig = visualisations(node_points, connections, best_path_nodes, best_path_edges)
+#draw_map(fig)
+#fig.show()
+print("ok")
+'''
--- log_and_control.py
+++ log_and_control.py
@@ -28,18 +28,24 @@ and prints it to the console. After 10s the application disconnects and exits.
 import logging
 import time
 import math
-from threading import Timer
+import threading
+from matplotlib import pyplot as plt
+#from threading import Timer
+
 
 import cflib.crtp  # noqa
 from cflib.crazyflie import Crazyflie
 from cflib.crazyflie.log import LogConfig
 from cflib.utils import uri_helper
 
+import dijkstra_algorithm as dijk
+import numpy
+
 uri = uri_helper.uri_from_env(default='radio://0/100/2M/E7E7E7E720')
+lock = threading.Lock()
 
 # Only output errors from the logging framework
 logging.basicConfig(level=logging.ERROR)
-
 
 class LoggingExample:
     """
@@ -103,9 +109,6 @@ class LoggingExample:
             print('Could not add Stabilizer log config, bad configuration.')
 
         # Start a timer to disconnect in 10s
-        #t = Timer(100, self._cf.close_link)
-        #t.start()
-
     def _stab_log_error(self, logconf, msg):
         """Callback from the log API when an error occurs"""
         print('Error when logging %s: %s' % (logconf.name, msg))
@@ -134,11 +137,50 @@ class LoggingExample:
         print('Disconnected from %s' % link_uri)
         self.is_connected = False
 
+not_detected = [True, True, True, True]
+start_looking_for_obstacles = False
+height_desired = 0.3
+obstacle_detection = 300
+length = 0.07
+rows = 28
+columns = 15
+start = 0
+goal = 415
+#dijk.print_msg()
+graph, node_points, connections, best_path_edges, best_path_nodes, fig, node_ids = dijk.generate_dijkstra(rows, columns, length, start, goal)
+visual = connections
+#fig = dijk.visualisations(node_points, connections, best_path_nodes, best_path_edges)
+
+def timer():
+    global not_detected
+    global graph, node_points, connections, best_path_edges, best_path_nodes, fig
+    while True:
+        #print("Selma dead and thread " + str(not_detected))
+        for i in range(len(not_detected)):
+            if(not not_detected[i]):
+                print("OBSTACLLLEEEEEEEE")
+                #connections = dijk.remove(graph, 6)
+                #if(connections != None):
+                    #plt.close(fig)
+                    #best_path_edges, best_path_nodes = dijk.get_best_path(graph, node_points, start, goal)
+                #dijk.draw_map(fig)
+                time.sleep(1)
+                print("done")
+                with lock:
+                    not_detected[i] = True
+
+        time.sleep(0.1)   # 3 seconds.
+
 def idle (cf, height, length):
     for y in range(length):
         cf.commander.send_hover_setpoint(0, 0, 0, height)
         time.sleep(0.1)
 
+def idle_at_point(cf, height, length, point):
+    for y in range(length):
+        cf.commander.send_position_setpoint(point[0], point[1], height, 0)
+        time.sleep(0.1)
+        
 def check_if_arrived(pos_bot, pos_goal, threshold):
     if(math.dist(pos_bot, pos_goal) < threshold):
         return True
@@ -148,12 +190,33 @@ def takeoff(cf):
     for y in range(10):
         cf.commander.send_hover_setpoint(0, 0, 0, y/25)
         time.sleep(0.1)
-    idle(cf, 0.4, 20)
+    idle(cf, height_desired, 20)
+
+def detect_obstacle(sensor, threshold):
+    if sensor < threshold:
+        return True
     
+def set_start_looking_for_obstacles():
+    global start_looking_for_obstacles
+    start_looking_for_obstacles = True
+
+def sensor_detected_obstacle(range_front, range_left, range_right, range_back):
+    #front, left, right, back
+    sensor = [False, False, False, False]
+ 
+    if detect_obstacle(range_front, obstacle_detection):
+        sensor[0] = True
+    if detect_obstacle(range_left, obstacle_detection):
+        sensor[1] = True
+    if detect_obstacle(range_right, obstacle_detection):
+        sensor[2] = True
+    if detect_obstacle(range_back, obstacle_detection):
+        sensor[3] = True
+    return sensor
+
 if __name__ == '__main__':
     # Initialize the low-level drivers
     cflib.crtp.init_drivers()
-
     le = LoggingExample(uri)
     cf = le._cf
 
@@ -162,20 +225,38 @@ if __name__ == '__main__':
     cf.param.set_value('kalman.resetEstimation', '0')
     time.sleep(2)
     print("Selma is a baddie")
-    time.sleep(5)
+    #time.sleep(5)
     # The Crazyflie lib doesn't contain anything to keep the application alive,
     # so this is where your application should do something. In our case we
     # are just waiting until we are disconnected.
     state  = 0
     seq_index = 0
     sequence = [
-    (0.5, 0, 0.4),
-    (0.5, 0.5, 0.4),
-    (0, 0.5, 0.4),
-    (0.0, 0.0, 0.4),
+    (0.5, 0, height_desired),
+    (0.5, 0.5, height_desired),
+    (0, 0.5, height_desired),
+    (0.0, 0.0, height_desired),
     ]
     x_past = 0
     y_past = 0
+    dijk.draw_map(fig)
+    #time.sleep(2)
+    #connections = dijk.remove(graph, 6)
+    #best_path_edges, best_path_nodes = dijk.get_best_path(graph, node_points, start, goal)
+    #dijk.visualisations(node_points, connections, best_path_nodes, best_path_edges)
+    #time.sleep(2)
+    converted_all_nodes = dijk.conversion(node_points, height_desired)
+    converted_nodes = dijk.conversion(best_path_nodes, height_desired)
+    print(sequence)
+    sequence = converted_nodes
+    print(sequence)
+    print(graph)
+    t = threading.Thread(target=timer)
+    t.start()
+    t2 = threading.Timer(5, set_start_looking_for_obstacles)
+    t2.start()
+    print("Going in")
+    last_index = 0
     while le.is_connected:
         x = le.states['stateEstimate.x']
         y = le.states['stateEstimate.y']
@@ -190,21 +271,101 @@ if __name__ == '__main__':
         if state == 0:
             takeoff(cf)
             state = 1
-
         if state == 1:
             cf.commander.send_position_setpoint(sequence[seq_index][0], sequence[seq_index][1], sequence[seq_index][2], 0)
             pos_bot = [x, y]
             pos_goal = [sequence[seq_index][0], sequence[seq_index][1]]
             if(check_if_arrived(pos_bot, pos_goal, 0.05)):
-                idle(cf, 0.4, 20)
+                #idle_at_point(cf, height_desired, 5, pos_goal)
+                last_index = node_ids[seq_index]
                 seq_index += 1
                 if (seq_index > len(sequence) - 1):
-                    seq_index = 0
-        print("x = {:.3f}, y = {:.3f}, z = {:.3f} ".format(x, y, z))
+                    seq_index =  len(sequence) - 1
+                    state = 2
+            obstacles_detected = sensor_detected_obstacle(range_front, range_left, range_right, range_back)
+            #print(obstacles_detected)
+            true_indices = [i for i, x in enumerate(obstacles_detected) if x]
+            true_not_detected = [i for i, x in enumerate(not_detected) if x] 
+            if(len(true_indices) > 0 and len(true_not_detected) > 0 and start_looking_for_obstacles):
+                #print("Detected")
+                for i in range(len(true_indices)):
+                    #connections = None
+                    print(i)
+                    print(true_indices)
+                    print(not_detected)
+                    if(not_detected[true_indices[i]]):
+                        #with lock:
+                            #not_detected[true_indices[i]] = False
+                        if(true_indices[i] == 0):
+                            obst_x = x + range_front/1000
+                            obst_y = y
+                        elif(true_indices[i] == 1):
+                            obst_x = x
+                            obst_y = y + range_left/1000
+                        elif(true_indices[i] == 2):
+                            print("HERE!")
+                            obst_x = x
+                            obst_y = y - range_right/1000
+                        elif(true_indices[i] == 3):
+                            obst_x = x - range_back/1000
+                            obst_y = y
+                        obstacle = numpy.array((obst_x, obst_y, height_desired))
+                        print("obstacle_value : " + str(obstacle))
 
-        if range_up < 200:
-            for y in range(10, -1, -1):
+                        distances = numpy.linalg.norm(converted_all_nodes-obstacle, axis=1)
+                        min_index = numpy.argmin(distances)
+                        print("Node to remove is ", + min_index) 
+
+                        connections = dijk.remove(graph, min_index)
+                        print("removed it")
+                        #print(graph)
+                        #del converted_all_nodes[min_index]
+                        if(connections != None):
+                            visual = connections
+                            #drone_pos = numpy.array(sequence[seq_index-1])
+                            #print(drone)
+                            #distances_bot = numpy.linalg.norm(converted_all_nodes-drone_pos, axis=1)
+                            #if (seq_index-1 > -1):
+                             #   print(node_ids)
+                             #   start_index_bot = node_ids[seq_index]
+                              #  if start_index_bot == min_index : start_index_bot = node_ids[seq_index-1]
+                            #else:
+                             #   start_index_bot = 0
+                            start_index_bot = last_index
+                            if (start_index_bot == min_index):
+                                start_index_bot = node_ids[seq_index - 2]
+                            print("start_index: " + str(start_index_bot))
+                            print("got fams position")
+                            #plt.close(fig)
+                            #print(node_points)
+                            cf.commander.send_hover_setpoint(0, 0, 0, height_desired)
+                            print("dijkstra")
+                            best_path_edges, best_path_nodes, node_ids = dijk.get_best_path(graph, node_points, start_index_bot, goal)
+                            print(best_path_nodes)
+                            seq_index = 0
+                            print("finna convert")
+                            converted_nodes = dijk.conversion(best_path_nodes, height_desired)
+                            print(converted_nodes)
+                            #valid = connections
+                            #plt.close()
+                            #fig = dijk.visualisations(node_points, visual, best_path_nodes, best_path_edges)
+                            #dijk.draw_map(fig)
+                            sequence = converted_nodes
+                #best_path_edges, best_path_nodes = dijk.get_best_path(graph, node_points, start, goal)
+                #plt = dijk.visualisations(node_points, connections, best_path_nodes, best_path_edges)
+        #print("x = {:.3f}, y = {:.3f}, z = {:.3f} ".format(x, y, z))
+        #print("Range Front " + str(range_front))
+        if range_up < 200 or state == 2:
+            print("Landing")
+            for y in range(5, -1, -1):
                 cf.commander.send_hover_setpoint(0, 0, 0, y/25)
                 time.sleep(0.1)
             cf.commander.send_stop_setpoint()
+            plt.close()
+            #print(visual)
+            #print(best_path_nodes)
+            fig = dijk.visualisations(node_points, visual, best_path_nodes, best_path_edges)
+            dijk.draw_map(fig)
+            #time.sleep(5)
+            print("finito")
             break
